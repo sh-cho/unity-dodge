ddde65858c55e6d45dbfbf35db5d891ae73a4721
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2021-07-28 19:31:05 +0900

    Add player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidBody;
+    [SerializeField] private float speed = 8.0f;
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(h, 0, v);
+        playerRigidBody.velocity = movement * speed;
+    }
+
+    // -----------------------------------------------------------------------------
+    // Custom methods
+    // -----------------------------------------------------------------------------
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
