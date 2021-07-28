ec10350736f35030916ba69e04f188e951feda1e
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2021-07-28 20:54:12 +0900

    Add bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 8.0f;
+    private Rigidbody bulletRigidbody;
+
+    private void Awake()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        // Destroy itself after 3 seconds
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
